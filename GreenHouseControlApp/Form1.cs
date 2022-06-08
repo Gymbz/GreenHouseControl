e688680ba23217c6a230f01d009ff92885562737
Mateusz Gorczyński <mati_gor@o2.pl>
2022-06-08 18:42:15 +0200

    Ukończono "jeżdzące" panele poszczególnych wyborów z menu.

--- GreenHouseControlApp/Form1.cs
+++ GreenHouseControlApp/Form1.cs
@@ -12,109 +12,17 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        int panelsWidth = 1000;
-        bool panelControlParametresHidden;
-        bool panelPreviewReportsHidden;
-        bool panelAppConfigHidden;
-
+        MultipleSlidingPanels p1, p2, p3;
         public Form1()
         {
             InitializeComponent();
 
-            panelControlParametres.Width = 0;
-            panelPreviewReports.Width = 0;
-            panelAppConfig.Width = 0;
+            p1 = new MultipleSlidingPanels(ref panelControlParametres, ref btnControlParametres, ref btnPreviewReports, ref btnAppConfig);
+            p2 = new MultipleSlidingPanels(ref panelPreviewReports, ref btnPreviewReports, ref btnControlParametres, ref btnAppConfig);
+            p3 = new MultipleSlidingPanels(ref panelAppConfig, ref btnAppConfig, ref btnControlParametres, ref btnPreviewReports);
 
-            panelControlParametresHidden = true;
-            panelPreviewReportsHidden = true;
-            panelAppConfigHidden = true;
         }
 
-        private void btnControlParametres_Click(object sender, EventArgs e)
-        {
-            btnControlParametres.BackColor = Color.Red;
-            panelControlParametresHidden = true;
-            timer1.Start();
-        }
 
-        private void btnPreviewReports_Click(object sender, EventArgs e)
-        {
-            btnPreviewReports.BackColor = Color.Red;
-            panelPreviewReportsHidden = true;
-            timer2.Start();
-        }
-
-        private void btnAppConfig_Click(object sender, EventArgs e)
-        {
-            btnAppConfig.BackColor = Color.Red;
-        }
-
-        private void btnControlParametres_Leave(object sender, EventArgs e)
-        {
-            btnControlParametres.BackColor = Color.FloralWhite;
-            panelControlParametresHidden = false;
-            timer1.Start();
-        }
-
-        private void btnPreviewReports_Leave(object sender, EventArgs e)
-        {
-            btnPreviewReports.BackColor = Color.FloralWhite; 
-            panelPreviewReportsHidden = false;
-            timer2.Start();
-        }
-
-        private void btnAppConfig_Leave(object sender, EventArgs e)
-        {
-            btnAppConfig.BackColor = Color.FloralWhite;
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            if (panelControlParametresHidden)
-            {
-                panelControlParametres.Width = panelControlParametres.Width + 20;
-                if (panelControlParametres.Width >= panelsWidth)
-                {
-                    timer1.Stop();
-                    panelControlParametresHidden = false;
-                    this.Refresh();
-                }
-            }
-
-            else
-            {
-                panelControlParametres.Width = panelControlParametres.Width - 20;
-                if(panelControlParametres.Width == 0 )
-                {
-                    timer1.Stop();
-                    panelControlParametresHidden = true;
-                    this.Refresh();
-                }
-            }
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            if (panelPreviewReportsHidden)
-            {
-                panelPreviewReports.Width = panelPreviewReports.Width + 20;
-                if (panelPreviewReports.Width >= panelsWidth)
-                {
-                    timer2.Stop();
-                    panelPreviewReportsHidden = false;
-                    this.Refresh();
-                }
-            }
-            else
-            {
-                panelPreviewReports.Width = panelPreviewReports.Width - 20;
-                if (panelPreviewReports.Width == 0)
-                {
-                    timer2.Stop();
-                    panelPreviewReportsHidden = true;
-                    this.Refresh();
-                }
-            }
-        }
     }
 }
---
+++ GreenHouseControlApp/MultipleSlidingPanels.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class MultipleSlidingPanels
+    {
+        Panel span;
+        Button button;
+        Button other_button1;
+        Button other_button2;
+
+        bool hidden;
+        bool extending;
+
+        Timer t;
+
+        public MultipleSlidingPanels(ref Panel p, ref Button b, ref Button ob1,ref Button ob2)
+        {
+            this.span = p;
+            this.button = b;
+            this.other_button1 = ob1;
+            this.other_button2 = ob2;
+
+            hidden = true;
+            extending = false;
+
+            p.Width = 0;
+
+            b.Click += new EventHandler(Button_clicked);
+            ob1.Click += new EventHandler(ButtonOther_clicked);
+            ob2.Click += new EventHandler(ButtonOther_clicked);
+
+
+            t = new Timer();
+            t.Interval = 10;
+            t.Tick += new EventHandler(Timer_tick);
+        }
+
+        private void ButtonOther_clicked(object sender, EventArgs e)
+        {
+            hidden = false;
+            t.Start();
+        }
+
+        void ChangeSize(int val)
+        {
+            span.Width += val;
+            if (span.Width >= 1000 || span.Width <= 0)
+            {
+                t.Stop();
+                hidden = !hidden;
+                extending = false;
+            }
+        }
+
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            if (hidden) ChangeSize(+50);
+            else ChangeSize(-50);
+        }
+
+        private void Button_clicked(object sender, EventArgs e)
+        {
+            if (extending)
+            {
+                t.Stop();
+                hidden = false;
+                t.Start();
+            }
+
+            t.Start();
+            extending = true;
+        }
+
+
+    }
+}
