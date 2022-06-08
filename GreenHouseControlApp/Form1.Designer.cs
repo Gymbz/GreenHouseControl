04a6e5837d52bd0a1e4c69c1c6297d219f56a112
Mateusz Gorczyński <mati_gor@o2.pl>
2022-06-08 19:56:26 +0200

    Ustawiono paletę kolorów, zrezygnowano z paska domyślnego i dodano "customowe" przyciski exit i fullscreen

--- GreenHouseControlApp/Form1.Designer.cs
+++ GreenHouseControlApp/Form1.Designer.cs
@@ -29,35 +29,46 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
             this.btnAppConfig = new System.Windows.Forms.Button();
             this.btnPreviewReports = new System.Windows.Forms.Button();
             this.btnControlParametres = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelUser = new System.Windows.Forms.Panel();
             this.panelControlParametres = new System.Windows.Forms.Panel();
+            this.labelTemperature = new System.Windows.Forms.Label();
             this.panelPreviewReports = new System.Windows.Forms.Panel();
             this.panelAppConfig = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.labelAppName = new System.Windows.Forms.Label();
+            this.TopBar = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            this.panelControlParametres.SuspendLayout();
+            this.TopBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelMenu
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.panel1.Controls.Add(this.btnAppConfig);
-            this.panel1.Controls.Add(this.btnPreviewReports);
-            this.panel1.Controls.Add(this.btnControlParametres);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(280, 681);
-            this.panel1.TabIndex = 0;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(212)))), ((int)(((byte)(190)))));
+            this.panelMenu.Controls.Add(this.btnAppConfig);
+            this.panelMenu.Controls.Add(this.btnPreviewReports);
+            this.panelMenu.Controls.Add(this.btnControlParametres);
+            this.panelMenu.Controls.Add(this.panelUser);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(280, 720);
+            this.panelMenu.TabIndex = 0;
             // 
             // btnAppConfig
             // 
-            this.btnAppConfig.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnAppConfig.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(212)))), ((int)(((byte)(190)))));
             this.btnAppConfig.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAppConfig.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAppConfig.FlatAppearance.BorderSize = 0;
+            this.btnAppConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAppConfig.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnAppConfig.Location = new System.Drawing.Point(0, 363);
             this.btnAppConfig.Name = "btnAppConfig";
@@ -68,9 +79,11 @@ namespace WindowsFormsApp1
             // 
             // btnPreviewReports
             // 
-            this.btnPreviewReports.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnPreviewReports.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(212)))), ((int)(((byte)(190)))));
             this.btnPreviewReports.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnPreviewReports.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPreviewReports.FlatAppearance.BorderSize = 0;
+            this.btnPreviewReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnPreviewReports.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnPreviewReports.Location = new System.Drawing.Point(0, 288);
             this.btnPreviewReports.Name = "btnPreviewReports";
@@ -81,9 +94,11 @@ namespace WindowsFormsApp1
             // 
             // btnControlParametres
             // 
-            this.btnControlParametres.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnControlParametres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(212)))), ((int)(((byte)(190)))));
             this.btnControlParametres.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnControlParametres.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnControlParametres.FlatAppearance.BorderSize = 0;
+            this.btnControlParametres.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnControlParametres.Font = new System.Drawing.Font("Montserrat", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.btnControlParametres.Location = new System.Drawing.Point(0, 213);
             this.btnControlParametres.Name = "btnControlParametres";
@@ -92,70 +107,142 @@ namespace WindowsFormsApp1
             this.btnControlParametres.Text = "Zmiana Parametrów";
             this.btnControlParametres.UseVisualStyleBackColor = false;
             // 
-            // panel2
+            // panelUser
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Info;
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(280, 213);
-            this.panel2.TabIndex = 1;
+            this.panelUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(212)))), ((int)(((byte)(190)))));
+            this.panelUser.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelUser.Location = new System.Drawing.Point(0, 0);
+            this.panelUser.Name = "panelUser";
+            this.panelUser.Size = new System.Drawing.Size(280, 213);
+            this.panelUser.TabIndex = 1;
             // 
             // panelControlParametres
             // 
-            this.panelControlParametres.BackColor = System.Drawing.Color.DarkRed;
+            this.panelControlParametres.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(174)))), ((int)(((byte)(230)))), ((int)(((byte)(203)))));
             this.panelControlParametres.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panelControlParametres.Location = new System.Drawing.Point(274, 0);
+            this.panelControlParametres.Controls.Add(this.labelTemperature);
+            this.panelControlParametres.Location = new System.Drawing.Point(280, 50);
             this.panelControlParametres.Name = "panelControlParametres";
-            this.panelControlParametres.Size = new System.Drawing.Size(1012, 681);
+            this.panelControlParametres.Size = new System.Drawing.Size(1000, 671);
             this.panelControlParametres.TabIndex = 1;
+            // 
+            // labelTemperature
+            // 
+            this.labelTemperature.AutoSize = true;
+            this.labelTemperature.Font = new System.Drawing.Font("Montserrat", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTemperature.Location = new System.Drawing.Point(41, 66);
+            this.labelTemperature.Name = "labelTemperature";
+            this.labelTemperature.Size = new System.Drawing.Size(176, 29);
+            this.labelTemperature.TabIndex = 0;
+            this.labelTemperature.Text = "TEMPERATURA";
             // 
             // panelPreviewReports
             // 
-            this.panelPreviewReports.BackColor = System.Drawing.Color.Orange;
+            this.panelPreviewReports.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.panelPreviewReports.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panelPreviewReports.Location = new System.Drawing.Point(274, 0);
+            this.panelPreviewReports.Location = new System.Drawing.Point(280, 50);
             this.panelPreviewReports.Name = "panelPreviewReports";
-            this.panelPreviewReports.Size = new System.Drawing.Size(1009, 698);
+            this.panelPreviewReports.Size = new System.Drawing.Size(1000, 670);
             this.panelPreviewReports.TabIndex = 2;
             // 
             // panelAppConfig
             // 
-            this.panelAppConfig.BackColor = System.Drawing.Color.GreenYellow;
+            this.panelAppConfig.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.panelAppConfig.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.panelAppConfig.Location = new System.Drawing.Point(274, 0);
+            this.panelAppConfig.Location = new System.Drawing.Point(280, 50);
             this.panelAppConfig.Name = "panelAppConfig";
-            this.panelAppConfig.Size = new System.Drawing.Size(1006, 771);
+            this.panelAppConfig.Size = new System.Drawing.Size(1000, 670);
             this.panelAppConfig.TabIndex = 3;
+            // 
+            // labelAppName
+            // 
+            this.labelAppName.AutoSize = true;
+            this.labelAppName.Font = new System.Drawing.Font("Montserrat", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelAppName.Location = new System.Drawing.Point(6, 9);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(321, 33);
+            this.labelAppName.TabIndex = 1;
+            this.labelAppName.Text = "GREENHOUSE CONTROL";
+            // 
+            // TopBar
+            // 
+            this.TopBar.Controls.Add(this.pictureBox2);
+            this.TopBar.Controls.Add(this.pictureBox1);
+            this.TopBar.Controls.Add(this.labelAppName);
+            this.TopBar.Location = new System.Drawing.Point(280, 0);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(1000, 50);
+            this.TopBar.TabIndex = 4;
+            this.TopBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseDown);
+            this.TopBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseMove);
+            this.TopBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBar_MouseUp);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.fullscreen_exit_116839;
+            this.pictureBox2.InitialImage = global::WindowsFormsApp1.Properties.Resources.fullscreen_exit_116839;
+            this.pictureBox2.Location = new System.Drawing.Point(932, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.exit_icon_icons_com_70975;
+            this.pictureBox1.Location = new System.Drawing.Point(963, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(223)))), ((int)(((byte)(216)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ControlBox = false;
+            this.Controls.Add(this.TopBar);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.panelControlParametres);
             this.Controls.Add(this.panelAppConfig);
             this.Controls.Add(this.panelPreviewReports);
-            this.Controls.Add(this.panelControlParametres);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = " ";
-            this.panel1.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelControlParametres.ResumeLayout(false);
+            this.panelControlParametres.PerformLayout();
+            this.TopBar.ResumeLayout(false);
+            this.TopBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelMenu;
         private System.Windows.Forms.Button btnControlParametres;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelUser;
         private System.Windows.Forms.Button btnAppConfig;
         private System.Windows.Forms.Button btnPreviewReports;
         private System.Windows.Forms.Panel panelControlParametres;
         private System.Windows.Forms.Panel panelPreviewReports;
         private System.Windows.Forms.Panel panelAppConfig;
+        private System.Windows.Forms.Label labelTemperature;
+        private System.Windows.Forms.Label labelAppName;
+        private System.Windows.Forms.Panel TopBar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
