8e2581b45234c32a934c1ec6feed9d596ae93022
Mateusz Gorczyński <mati_gor@o2.pl>
2022-06-09 15:59:01 +0200

    Kilka zmian estetycznych

--- GreenHouseControlApp/Form1.cs
+++ GreenHouseControlApp/Form1.cs
@@ -13,13 +13,31 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         MultipleSlidingPanels p1, p2, p3;
+        ButtonHover pB1;
+
         int mouseX = 0;
         int mouseY = 0;
+        int mouseinX;
+        int mouseinY;
         bool mouseDown;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateHour.Text = DateTime.Now.ToString();
+        }
 
         private void TopBar_MouseDown(object sender, MouseEventArgs e)
         {
             mouseDown = true;
+
+            mouseinX = MousePosition.X - Bounds.X;
+            mouseinY = MousePosition.Y - Bounds.Y;
         }
 
         private void TopBar_MouseUp(object sender, MouseEventArgs e)
@@ -27,17 +45,43 @@ namespace WindowsFormsApp1
             mouseDown = false;
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+
+        private void labelAppName_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+
+            mouseinX = MousePosition.X - Bounds.X;
+            mouseinY = MousePosition.Y - Bounds.Y;
+        }
+
+        private void labelAppName_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                mouseX = MousePosition.X - mouseinX;
+                mouseY = MousePosition.Y - mouseinY;
+
+                this.SetDesktopLocation(mouseX, mouseY);
+            }
+        }
+
+        private void labelAppName_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
 
         private void TopBar_MouseMove(object sender, MouseEventArgs e)
         {
             if (mouseDown)
             {
-                mouseX = MousePosition.X -600;
-                mouseY = MousePosition.Y - 20;
+                mouseX = MousePosition.X - mouseinX;
+                mouseY = MousePosition.Y - mouseinY;
 
                 this.SetDesktopLocation(mouseX, mouseY);
             }
@@ -51,6 +95,7 @@ namespace WindowsFormsApp1
             p1 = new MultipleSlidingPanels(ref panelControlParametres, ref btnControlParametres, ref btnPreviewReports, ref btnAppConfig);
             p2 = new MultipleSlidingPanels(ref panelPreviewReports, ref btnPreviewReports, ref btnControlParametres, ref btnAppConfig);
             p3 = new MultipleSlidingPanels(ref panelAppConfig, ref btnAppConfig, ref btnControlParametres, ref btnPreviewReports);
+            pB1 = new ButtonHover(ref ExitButton);
 
         }
 
